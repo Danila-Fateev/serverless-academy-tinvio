445f72141e63150e9d772449d26a21a430841972
Вадим Фатеев <fateevdanon@gmail.com>
2023-03-08 10:29:13 +0100

    Create colors.js

---
+++ my-app/src/variables/colors.js
@@ -0,0 +1,25 @@
+const colors = {
+  textColor: {
+    primary: "#FF474D",
+    second: "#5C5C5C",
+    main: "#212121",
+    white: "#FFFFFF",
+    placeholder: "#BDBDBD",
+  },
+  inputsColor: "#F3F4F5",
+  backgroundsColor: {
+    grey: "#F6F6F6",
+    black: "#363636",
+  },
+  backgroundElements: {
+    radialSixtyPercent: "radial-gradient(#999999, #FAFAFA)",
+    radialFifteenPercent: "radial-gradient(#DADADA, #FAFAFA)",
+    diamondVisual: "#FB242B",
+  },
+  buttons: {
+    primary: "#FF474D",
+    second: "#FFFFFF",
+    textButton: "#212121",
+  },
+  shadow: "#212121",
+};
